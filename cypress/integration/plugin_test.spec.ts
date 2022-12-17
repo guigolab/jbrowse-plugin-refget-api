327ee57f0671eb4e96f55ea9fadcb00be701e0b2
Emilio Righi <righiemilio@gmail.com>
2022-12-17 20:02:09 +0100

    add cypress test

--- cypress/integration/plugin_test.spec.ts
+++ cypress/integration/plugin_test.spec.ts
@@ -2,15 +2,15 @@ describe('My First Test', () => {
   it('visits JBrowse', () => {
     // You can put JBrowse 2 into any session you want this way at the beginning
     // of your test!
-    cy.fixture('hello_view.json').then(sessionData => {
+    cy.fixture('refget_session.json').then(sessionData => {
       cy.writeFile(
-        '.jbrowse/hello_view.json',
+        '.jbrowse/refget_session.json',
         JSON.stringify(sessionData, null, 2),
       )
-      cy.visit('/?config=hello_view.json')
+      cy.visit('/?config=refget_session.json')
 
       // The plugin successfully loads
-      cy.contains('Hello plugin developers!')
+      cy.contains('Hello refget plugin')
     })
   })
 })
